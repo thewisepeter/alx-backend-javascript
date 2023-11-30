c1993488ed2871293aee08860bf644e40d8be6c2
thewisepeter <rempeterwisdom@gmail.com>
2023-11-30 18:39:33 +0000

    solution to 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,9 +1,9 @@
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   // getters
@@ -12,13 +12,14 @@ class SkyHighBuilding extends Building {
   }
 
   // setters
-  set floors(floors) {
-    if (typeof floors !== 'number') {
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
       throw new TypeError('Floors must be a number');
     }
-    this._floors = floors;
+    this._floors = newFloors;
   }
 
+  // Overide method
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`;
   }
