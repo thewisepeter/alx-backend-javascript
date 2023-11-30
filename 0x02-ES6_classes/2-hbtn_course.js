b63c68dc4c232b5616ceaac5c52c84a5f2b80af9
thewisepeter <rempeterwisdom@gmail.com>
2023-11-30 11:40:45 +0000

    solution to 2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,33 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    this._students = newStudents;
+  }
+}
