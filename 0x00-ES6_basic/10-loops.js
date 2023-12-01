8b5f56a33f5ced12aac6f85b9277c380f6ad4484
thewisepeter <rempeterwisdom@gmail.com>
2023-12-01 07:30:53 +0000

    solution to 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
-    const idx = array.indexOf(value);
-    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
+  const array1 = [];
+  for (const item of array) {
+    array1.push(appendString + item);
   }
 
-  return array;
+  return array1;
 }
