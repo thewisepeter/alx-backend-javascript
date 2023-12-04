fe3c462250b57cfb177a9ec914d1b4454fc69839
thewisepeter <rempeterwisdom@gmail.com>
2023-12-04 13:57:23 +0000

    solution to 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,10 @@ export default function getStudentIdsSum(arrayStudents) {
   if (Array.isArray(arrayStudents)) {
     const studentId = arrayStudents.map((student) => student.id);
     const initialValue = 0;
-    return studentId.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+    return studentId.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      initialValue,
+    );
   }
   return 0;
 }
