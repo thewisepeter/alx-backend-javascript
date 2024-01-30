70c5796e5e5e6259180d32d53d2949ae2f5f2a06
thewisepeter <rempeterwisdom@gmail.com>
2024-01-30 13:54:25 +0300

    solution to 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -9,7 +9,7 @@ const rl = readline.createInterface({
 });
 
 rl.question('Your name is: ', (name) => {
-  `Your name is: ${name}`;
+  console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   rl.close();
 });
