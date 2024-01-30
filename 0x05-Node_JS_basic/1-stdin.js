56ee33747b1937125ea0e28b1894f980a1c3f749
thewisepeter <rempeterwisdom@gmail.com>
2024-01-30 15:07:05 +0300

    solution to 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,7 +8,23 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Your name is: ', () => {
-  console.log('This important software is now closing');
-  rl.close();
+rl.on('line', (name) => {
+  if (name && name.trim() !== '') {
+    if (process.stdin.isTTY) {
+      console.log(`Your name is: ${name}`);
+    }
+    if (!process.stdin.isTTY) {
+      console.log('Welcome to Holberton School, what is your name?');
+      console.log(`Your name is: ${name}`);
+      console.log('This important software is now closing');
+    }
+    rl.close();
+  }
+});
+
+// Listen for the close event
+rl.on('close', () => {
+  if (process.stdin.isTTY) {
+    process.exit();
+  }
 });
