b8e522ac183f81ceb1b592b29907b22083c21d0b
thewisepeter <rempeterwisdom@gmail.com>
2024-01-30 15:18:37 +0300

    solution to 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
-  if (process.stdin.isTTY) {
-    process.stdout.write('\nThis important software is now closing\n');
+
+  // Check if input is coming from a terminal (TTY)
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
   }
+
   process.exit();
 });
