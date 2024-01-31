c95620713cce1f88c059ca40be0a33e4d78f3633
thewisepeter <rempeterwisdom@gmail.com>
2024-01-31 12:50:23 +0300

    solution to 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,40 +4,32 @@ const { readFile } = require('fs');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-function countStudents(path) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+function countStudents(fileName) {
   return new Promise((resolve, reject) => {
-    readFile(path, (error, data) => {
+    fs.readFile(fileName, 'utf-8', (error, data) => {
       if (error) {
-        reject(Error('Cannot load the database'));
+        reject(new Error('Cannot load the database'));
       } else {
-        const lines = data.toString().split('\n');
-        for (let i = 0; i < lines.length; i += 1) {
-          if (lines[i]) {
-            length += 1;
-            const field = lines[i].toString().split(',');
-            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-              students[field[3]].push(field[0]);
-            } else {
-              students[field[3]] = [field[0]];
-            }
-            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-              fields[field[3]] += 1;
-            } else {
-              fields[field[3]] = 1;
-            }
+        const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
+        const students = {};
+        const fields = {};
+        lines.forEach((line) => {
+          const fields = line.split(',');
+          const name = fields[0];
+          const field = fields[3];
+          if (!students[field]) {
+            students[field] = [];
           }
-        }
-        const l = length - 1;
-        console.log(`Number of students: ${l}`);
+          students[field].push(name);
+          fields[field] = (fields[field] || 0) + 1;
+        });
+
+        const output = [];
+        output.push(`Number of students: ${lines.length}`);
         for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
-          }
+          output.push(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
         }
-        resolve(data);
+        resolve(output.join('\n').trim()); // Remove trailing newline character
       }
     });
   });
