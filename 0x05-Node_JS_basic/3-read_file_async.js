524ea4163363752f57d9416f36c8b888430e055d
thewisepeter <rempeterwisdom@gmail.com>
2024-01-31 11:06:55 +0300

    solution to 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,34 +1,39 @@
-const fs = require('fs');
+const { readFile } = require('fs');
 
 function countStudents(path) {
+  const students = {};
+  const fields = {};
+  let length = 0;
   return new Promise((resolve, reject) => {
-    // Attempt to read the database file asynchronously
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        // If the database is not available, reject the Promise with an error
-        reject(new Error('Cannot load the database'));
+    readFile(path, (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
       } else {
-        // If the database is available, process the data and resolve the Promise
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const students = {};
-        const fields = {};
-
-        lines.forEach((line) => {
-          const [name, field] = line.split(',');
-          if (!students[field]) students[field] = [];
-          students[field].push(name);
-
-          fields[field] = (fields[field] || 0) + 1;
-        });
-
-        const totalStudents = lines.length;
-        console.log(`Number of students: ${totalStudents}`);
-
-        for (const [field, count] of Object.entries(fields)) {
-          console.log(`Number of students in ${field}: ${count}. List: ${students[field].join(', ')}`);
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
         }
-
-        resolve();
+        const l = length - 1;
+        console.log(`Number of students: ${l}`);
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+          }
+        }
+        resolve(data);
       }
     });
   });
