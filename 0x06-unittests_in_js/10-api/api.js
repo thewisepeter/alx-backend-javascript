086f75a77383ebc9b89db1915f2ef417e9ceefd6
thewisepeter <rempeterwisdom@gmail.com>
2024-03-18 11:23:02 +0300

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const port = 7865;
 
@@ -8,26 +10,26 @@ app.get('/', function (req, res) {
 });
 
 app.get('/cart/:id([0-9]+)', function (req, res) {
-    const cartId = req.params.id;
-    if (!cartId) {
-      res.status(400).send('Cart ID is required');
-    } else {
-      res.send(`Payment methods for cart ${cartId}`);
-    }
+  const cartId = req.params.id;
+  if (!cartId) {
+    res.status(400).send('Cart ID is required');
+  } else {
+    res.send(`Payment methods for cart ${cartId}`);
+  }
 });
 
 app.get('/available_payments', (req, res) => {
-    res.set('Content-Type', 'application/json');
-    res.send({
-      payment_methods: {
-        credit_cards: true,
-        paypal: false
-      }
-    });
+  res.set('Content-Type', 'application/json');
+  res.send({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false
+    }
+  });
 });
 
 app.post('/login', (req, res) => {
-    res.send(`Welcome ${req.body.userName}`);
+  res.send(`Welcome ${req.body.userName}`);
 });
 
 app.listen(port, () => {
