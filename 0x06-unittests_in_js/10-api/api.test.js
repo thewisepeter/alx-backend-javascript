b33a61890ecd9241412c083cf880bea4c3edadef
thewisepeter <rempeterwisdom@gmail.com>
2024-03-18 11:16:56 +0300

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -80,99 +80,100 @@ describe('Cart Page', () => {
 });
 
 describe('/available_payments', () => {
-    it('should return status code 200', () => {
-      request.get('http://localhost:7865/available_payments', (error, res, body) => {
-        expect(res.statusCode).to.be.equal(200);
-      });
+  it('should return status code 200', () => {
+    request.get('http://localhost:7865/available_payments', (error, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
     });
+  });
   
-    it("should return body content 'Welcome to the payment system'", () => {
-      request.get('http://localhost:7865/available_payments', (error, res, body) => {
-        expect(JSON.parse(body)).to.deep.equal(
-          { payment_methods: { credit_cards: true, paypal: false } });
-      });
+  it("should return body content 'Welcome to the payment system'", () => {
+    request.get('http://localhost:7865/available_payments', (error, res, body) => {
+      expect(JSON.parse(body)).to.deep.equal(
+        { payment_methods: { credit_cards: true, paypal: false } });
     });
+  });
   
-    it('should have the correct content type', () => {
-      request('http://localhost:7865/available_payments', (error, res, body) => {
-        expect(res.headers['content-type']).to.equal('application/json; charset=utf-8');
-      });
+  it('should have the correct content type', () => {
+    request('http://localhost:7865/available_payments', (error, res, body) => {
+      expect(res.headers['content-type']).to.equal('application/json; charset=utf-8');
     });
+  });
   
-    it('should have the correct content length', () => {
-      request('http://localhost:7865/available_payments', (error, res, body) => {
-        expect(res.headers['content-length']).to.equal('56');
-      });
+  it('should have the correct content length', () => {
+    request('http://localhost:7865/available_payments', (error, res, body) => {
+      expect(res.headers['content-length']).to.equal('56');
     });
+  });
 });
   
 describe('/login', () => {
-    it('should return status code of 200', () => {
-      request.post({
-        url: 'http://localhost:7865/login',
-        form: { userName: 'Betty' }
-      }, (error, res, body) => {
-        expect(res.statusCode).to.be.equal(200);
-      });
+  it('should return status code of 200', () => {
+    request.post({
+      url: 'http://localhost:7865/login',
+      form: { userName: 'Betty' }
+    }, (error, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
     });
-    it('should return Welcome Betty', () => {
-      request.post({
-        url: 'http://localhost:7865/login',
-        json: { userName: 'Betty' }
-      }, (error, res, body) => {
-        expect(res.body).to.be.equal('Welcome Betty');
-      });
+  });
+
+  it('should return Welcome Betty', () => {
+    request.post({
+      url: 'http://localhost:7865/login',
+      json: { userName: 'Betty' }
+    }, (error, res, body) => {
+      expect(res.body).to.be.equal('Welcome Betty');
     });
+  });
   
-    it('should return the correct result with form data value', () => {
-      const data = {
-        userName: 'Betty',
-      };
-      request.post({
-        url: 'http://localhost:7865/login',
-        body: data,
-        json: true,
-      }, (error, res, body) => {
-        expect(body).to.contain('Welcome Betty');
-      });
+  it('should return the correct result with form data value', () => {
+    const data = {
+      userName: 'Peter',
+    };
+    request.post({
+      url: 'http://localhost:7865/login',
+      body: data,
+      json: true,
+    }, (error, res, body) => {
+      expect(body).to.contain('Welcome Peter');
     });
+  });
   
-    it('should return the correct content type', () => {
-      const data = {
-        userName: 'Betty',
-      };
-      request.post({
-        url: 'http://localhost:7865/login',
-        body: data,
-        json: true,
-      }, (error, res, body) => {
-        expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
-      });
+  it('should return the correct content type', () => {
+    const data = {
+      userName: 'Peter',
+    };
+    request.post({
+      url: 'http://localhost:7865/login',
+      body: data,
+      json: true,
+    }, (error, res, body) => {
+      expect(res.headers['content-type']).to.equal('text/html; charset=utf-8');
     });
+  });
   
-    it('should return the correct content length', () => {
-      const data = {
-        userName: 'Betty',
-      };
-      request.post({
-        url: 'http://localhost:7865/login',
-        body: data,
-        json: true,
-      }, (error, res, body) => {
-        expect(res.headers['content-length']).to.equal('13');
-      });
+  it('should return the correct content length', () => {
+    const data = {
+      userName: 'Betty',
+    };
+    request.post({
+      url: 'http://localhost:7865/login',
+      body: data,
+      json: true,
+    }, (error, res, body) => {
+      expect(res.headers['content-length']).to.equal('13');
     });
+  });
   
-    it('should return the correct status 404 with invalid get value', () => {
-      const data = {
-        username: 'Betty',
-      };
-      request.post({
-        url: 'http://localhost:7865/login',
-        body: data,
-        json: true,
-      }, (error, res, body) => {
-        expect(res.statusCode).to.equal(404);
-      });
+  it('should return the correct status 404 with invalid get value', () => {
+    const data = {
+      username: 'Betty',
+    };
+    request.post({
+      url: 'http://localhost:7865/login',
+      body: data,
+      json: true,
+    }, (error, res, body) => {
+      expect(res.statusCode).to.equal(404);
     });
+  });
 });
